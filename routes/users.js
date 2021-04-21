3436453ff7433b23882d5af03a5dedda278d4339
saleh-havid <saleh.havid@gmail.com>
2021-04-21 22:11:22 +0700

    finish auth part

--- routes/users.js
+++ routes/users.js
@@ -15,9 +15,11 @@ router.post(
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      console.log(registeredUser);
-      req.flash("success", "Welcome to Yelp Camp!");
-      res.redirect("/campgrounds");
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to Yelp Camp!");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
@@ -37,8 +39,16 @@ router.post(
   }),
   (req, res) => {
     req.flash("success", "Welcome Back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
   }
 );
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Good Bye!");
+  res.redirect("/campgrounds");
+});
 
 module.exports = router;
