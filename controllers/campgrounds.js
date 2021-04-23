022f0ec8188f241e8dfa3c9ab7799def61a85dce
saleh-havid <saleh.havid@gmail.com>
2021-04-23 22:39:54 +0700

    add bootstrap style to file input

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -49,6 +49,12 @@ module.exports.updateCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+  console.log(campground)
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  console.log(campground)
+  await campground.save();
+  console.log(campground)
   req.flash("success", "Successfully updated campground");
   res.redirect(`/campgrounds/${campground._id}`);
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,6 +28,7 @@ router
   .put(
     isLoggedin,
     isAuthor,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.updateCampground)
   )
