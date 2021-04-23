cfa2493edccf4233e34614cd41181f741daf3db5
saleh-havid <saleh.havid@gmail.com>
2021-04-23 16:18:18 +0700

    add local image upload with multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,14 +6,21 @@ const { isLoggedin, isAuthor, validateCampground } = require("../middleware");
 const review = require("../models/review");
 const campgrounds = require("../controllers/campgrounds");
 
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedin,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedin,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("it worked!");
+  });
 
 router.get("/new", isLoggedin, campgrounds.renderNewForm);
 
